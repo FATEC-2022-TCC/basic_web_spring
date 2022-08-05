733c570c00c9fdec622948d9d1a609b2f15d5d1f
Donizete Vida <donizetevida1998@outlook.com>
2022-08-05 19:41:44 -0300

    Basic query param working

--- src/main/kotlin/com/web/basic/Controller.kt
+++ src/main/kotlin/com/web/basic/Controller.kt
@@ -1,10 +1,22 @@
 package com.web.basic
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicInteger
 
 @RestController
 object Controller {
+    private val idGenerator = AtomicInteger(0)
+
     @GetMapping
     fun helloWorld() = "Hello World"
+
+    @GetMapping("data")
+    fun data(
+        @RequestParam(defaultValue = "no content") content: String
+    ) = Data(
+        id = idGenerator.incrementAndGet(),
+        content = content
+    )
 }
