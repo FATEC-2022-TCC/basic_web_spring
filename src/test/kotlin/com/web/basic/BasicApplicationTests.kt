dcadf8dcf5d3249e96b6f6c57bb4ca18ed493a07
Donizete Vida <donizetevida1998@outlook.com>
2022-08-05 19:34:00 -0300

    Initial project files

---
+++ src/test/kotlin/com/web/basic/BasicApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.web.basic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
